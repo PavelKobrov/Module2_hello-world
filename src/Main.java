6cc6b0bff411994372619f6aeb4c37f765ee0513
PavelKobrov <Pavel-kobrov@yandex.ru>
2021-12-06 10:06:40 +0300

    Hello world app

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+//  System.out.println(i);
+// i++;
+//  System.out.println(i);
+//  i++;
+//  System.out.println(i);
+//  System.out.println("Hello World!");
+
+//        int a = 1;
+//        int b = 2;
+//   int c = a + b;
+// System.out.println("sum = " + c);
+//   System.out.println("a>b " + (a > b));
+//   System.out.println("a<b " + (a < b));
+    String hello ="Hello";
+    String world = "w   orld";
+        System.out.println(hello + " " + world + "!");
+
+    }
+}
